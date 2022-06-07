1ed2891b9c20d95851b488418ea2547c1469ba49
Pablo Jiménez Pascual <pablo@jimpas.me>
2022-06-08 01:24:24 +0200

    Bump dependency to flutter_map 1.0.0
    Fixes breaking changes introduced since the last major flutter_map
    release. Primarily instances of Stream<Null> used in the plugin API now
    have become Stream<void>.
    
    While I was here I've also added an analysis_options.yaml file using
    flutter_lints and fix some static analysis errors found, hopefully I
    didn't break something. I've also improved a little bit the formatting
    and wording of the REAME, mostly cosmetic changes.
    
    To release a new version bump it in the pubspec.yaml and update the
    changelog when it's ready.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # flutter_map_dragmarker
-A drag marker for flutter_map
 
-See the main.dart file for example use, but the example below should show pretty much everything.
+A drag marker for [`flutter_map`](https://github.com/fleaflet/flutter_map/)
 
-If using flutter_map 0.14.0+ then you probably need allowPanningOnScrollingParent: false, in MapOptions
+See the `main.dart` file in the `example` folder for usage, but the included example below in this file should show pretty much everything.
+
+If you are using `flutter_map` version 0.14.0 or higher then you probably need to set `allowPanningOnScrollingParent: false`, in `MapOptions`
 
 Note: This layer should probably be the last layer IF it needs to be dragged (otherwise why would you use this :)), otherwise other layers may intercept the gestures.
 
@@ -11,20 +12,18 @@ Add the `DragMarkerPlugin(),` to the plugins list, and then reference `DragMarke
 
 Most options should be self-explanatory. Offsets are just there for tweaking icon/images if they aren't centered, it may need a further tweak in code if there are some oddities like changing the bounds.
 
-feedbackBuilder and feedbackOffset are for a replacement widget/effect when actually dragging.
+`feedbackBuilder` and `feedbackOffset` are for a replacement widget/effect when actually dragging.
 
-height is mainly for Images, use size for Icons, but be aware internal calculations are based on size.
+`height` is mainly for `Images`, use `size` for `Icons`, but be aware internal calculations are based on size.
 
-rotateMarker decides whether to rotate in the opposite direction to the map rotation, keeping markers upright.
+`rotateMarker` decides whether to rotate in the opposite direction to the map rotation, keeping markers upright.
 
 Experimental automoving the map when near the edge is implemented (optional), but if anyone wants to improve it, pull requests welcome!
 
 Also may be interesting to move the map alternately to marker, so the marker stays in place, but then technically you aren't
 dragging any more, so thoughts welcome.
 
-
 ```dart
-
 FlutterMap(
   options: MapOptions(
     allowPanningOnScrollingParent: false, /// IMPORTANT for dragging
@@ -72,5 +71,4 @@ FlutterMap(
       ),
   ],
 ),
-
 ```
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -6,12 +5,14 @@ import 'package:flutter_map/plugin_api.dart';
 import 'dragmarker.dart';
 
 void main() {
-  runApp(TestApp());
+  runApp(const TestApp());
 }
 
 class TestApp extends StatefulWidget {
+  const TestApp({Key? key}) : super(key: key);
+
   @override
-  _TestAppState createState() => _TestAppState();
+  State<TestApp> createState() => _TestAppState();
 }
 
 class _TestAppState extends State<TestApp> {
@@ -25,51 +26,57 @@ class _TestAppState extends State<TestApp> {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Container(
-            child: FlutterMap(
-              options: MapOptions(
-                plugins: [
-                  DragMarkerPlugin(),
-                ],
-                center: LatLng(45.5231, -122.6765),
-                zoom: 6.4,
-              ),
-              layers: [
-                TileLayerOptions(
-                    urlTemplate:
-                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    subdomains: ['a', 'b', 'c']),
-                DragMarkerPluginOptions(
-                  markers: [
-                    DragMarker(
-                      point: LatLng(45.2131, -122.6765),
-                      width: 80.0,
-                      height: 80.0,
-                      offset: Offset(0.0, -8.0),
-                      builder: (ctx) => Container( child: Icon(Icons.location_on, size: 50) ),
-                      onDragStart:  (details,point) => print("Start point $point"),
-                      onDragEnd:    (details,point) => print("End point $point"),
-                      onDragUpdate: (details,point) {},
-                      onTap:        (point) { print("on tap"); },
-                      onLongPress:  (point) { print("on long press"); },
-                      feedbackBuilder: (ctx) => Container( child: Icon(Icons.edit_location, size: 75) ),
-                      feedbackOffset: Offset(0.0, -18.0),
-                      updateMapNearEdge: true,
-                      nearEdgeRatio: 2.0,
-                      nearEdgeSpeed: 1.0,
-                    ),
-                    DragMarker(
-                      point: LatLng(45.535, -122.675),
-                      width: 80.0,
-                      height: 80.0,
-                      builder: (ctx) => Container( child: Icon(Icons.location_on, size: 50) ),
-                      onDragEnd: (details,point) { print('Finished Drag $details $point'); },
-                      updateMapNearEdge: false,
-                    )
-                  ],
-                ),
+          child: FlutterMap(
+            options: MapOptions(
+              plugins: [
+                DragMarkerPlugin(),
               ],
+              center: LatLng(45.5231, -122.6765),
+              zoom: 6.4,
             ),
+            layers: [
+              TileLayerOptions(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c']),
+              DragMarkerPluginOptions(
+                markers: [
+                  DragMarker(
+                    point: LatLng(45.2131, -122.6765),
+                    width: 80.0,
+                    height: 80.0,
+                    offset: const Offset(0.0, -8.0),
+                    builder: (ctx) => const Icon(Icons.location_on, size: 50),
+                    onDragStart: (details, point) =>
+                        print("Start point $point"),
+                    onDragEnd: (details, point) => print("End point $point"),
+                    onDragUpdate: (details, point) {},
+                    onTap: (point) {
+                      print("on tap");
+                    },
+                    onLongPress: (point) {
+                      print("on long press");
+                    },
+                    feedbackBuilder: (ctx) =>
+                        const Icon(Icons.edit_location, size: 75),
+                    feedbackOffset: const Offset(0.0, -18.0),
+                    updateMapNearEdge: true,
+                    nearEdgeRatio: 2.0,
+                    nearEdgeSpeed: 1.0,
+                  ),
+                  DragMarker(
+                    point: LatLng(45.535, -122.675),
+                    width: 80.0,
+                    height: 80.0,
+                    builder: (ctx) => const Icon(Icons.location_on, size: 50),
+                    onDragEnd: (details, point) {
+                      print('Finished Drag $details $point');
+                    },
+                    updateMapNearEdge: false,
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
