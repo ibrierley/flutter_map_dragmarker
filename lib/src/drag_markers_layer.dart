adbb53904d3ca1ff989da16973b752f25fa2a84f
josxha <34318751+josxha@users.noreply.github.com>
2023-05-10 19:52:10 +0200

    convert DragMarkers layer widget to stateless

--- lib/src/drag_markers_layer.dart
+++ lib/src/drag_markers_layer.dart
@@ -4,30 +4,25 @@ import 'package:flutter_map/plugin_api.dart';
 import 'drag_marker.dart';
 import 'drag_marker_widget.dart';
 
-class DragMarkers extends StatefulWidget {
+class DragMarkers extends StatelessWidget {
   final List<DragMarker> markers;
 
   const DragMarkers({super.key, this.markers = const []});
 
   @override
-  State<DragMarkers> createState() => DragMarkersState();
-}
-
-class DragMarkersState extends State<DragMarkers> {
-  @override
   Widget build(BuildContext context) {
     final mapState = FlutterMapState.maybeOf(context) ??
         (throw StateError(
             '`DragMarkers` is a map layer and should not be build outside '
-            'a `FlutterMap` context.'));
+                'a `FlutterMap` context.'));
     return Stack(
-      children: widget.markers
+      children: markers
           .where((marker) => _boundsContainsMarker(mapState, marker))
           .map((marker) => DragMarkerWidget(
-                key: marker.key,
-                mapState: mapState,
-                marker: marker,
-              ))
+        key: marker.key,
+        mapState: mapState,
+        marker: marker,
+      ))
           .toList(growable: false),
     );
   }
